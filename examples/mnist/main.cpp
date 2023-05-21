7d57af9d15967009d4102bc4604645ce346bf180
Georgi Gerganov <ggerganov@gmail.com>
2023-05-21 16:00:09 +0300

    mnist : add tensor names

--- examples/mnist/main.cpp
+++ examples/mnist/main.cpp
@@ -57,24 +57,21 @@ bool mnist_model_load(const std::string & fname, mnist_model & model) {
     }
 
     auto & ctx = model.ctx;
+
     size_t ctx_size = 0;
 
     {
         const auto & hparams = model.hparams;
 
-        const int n_input  = hparams.n_input;
-        const int n_hidden = hparams.n_hidden;
+        const int n_input   = hparams.n_input;
+        const int n_hidden  = hparams.n_hidden;
         const int n_classes = hparams.n_classes;
 
-        // fc1 weight
-        ctx_size += n_input * n_hidden * ggml_type_sizef(GGML_TYPE_F32);
-        // fc1 bias
-        ctx_size += n_hidden * ggml_type_sizef(GGML_TYPE_F32);
+        ctx_size += n_input * n_hidden * ggml_type_sizef(GGML_TYPE_F32); // fc1 weight
+        ctx_size +=           n_hidden * ggml_type_sizef(GGML_TYPE_F32); // fc1 bias
 
-        // fc2 weight
-        ctx_size += n_hidden * n_classes * ggml_type_sizef(GGML_TYPE_F32);
-        // fc2 bias
-        ctx_size += n_classes * ggml_type_sizef(GGML_TYPE_F32);
+        ctx_size += n_hidden * n_classes * ggml_type_sizef(GGML_TYPE_F32); // fc2 weight
+        ctx_size +=            n_classes * ggml_type_sizef(GGML_TYPE_F32); // fc2 bias
 
         printf("%s: ggml ctx size = %6.2f MB\n", __func__, ctx_size/(1024.0*1024.0));
     }
@@ -100,25 +97,31 @@ bool mnist_model_load(const std::string & fname, mnist_model & model) {
         int32_t n_dims;
         fin.read(reinterpret_cast<char *>(&n_dims), sizeof(n_dims));
 
-        int32_t ne_weight[2] = { 1, 1 };
-        for (int i = 0; i < n_dims; ++i) {
-            fin.read(reinterpret_cast<char *>(&ne_weight[i]), sizeof(ne_weight[i]));
+        {
+            int32_t ne_weight[2] = { 1, 1 };
+            for (int i = 0; i < n_dims; ++i) {
+                fin.read(reinterpret_cast<char *>(&ne_weight[i]), sizeof(ne_weight[i]));
+            }
+
+            // FC1 dimensions taken from file, eg. 768x500
+            model.hparams.n_input  = ne_weight[0];
+            model.hparams.n_hidden = ne_weight[1];
+
+            model.fc1_weight = ggml_new_tensor_2d(ctx, GGML_TYPE_F32, model.hparams.n_input, model.hparams.n_hidden);
+            fin.read(reinterpret_cast<char *>(model.fc1_weight->data), ggml_nbytes(model.fc1_weight));
+            ggml_set_name(model.fc1_weight, "fc1_weight");
         }
 
-        // FC1 dimensions taken from file, eg. 768x500
-        model.hparams.n_input = ne_weight[0];
-        model.hparams.n_hidden = ne_weight[1];
+        {
+            int32_t ne_bias[2] = { 1, 1 };
+            for (int i = 0; i < n_dims; ++i) {
+                fin.read(reinterpret_cast<char *>(&ne_bias[i]), sizeof(ne_bias[i]));
+            }
 
-        model.fc1_weight     = ggml_new_tensor_2d(ctx, GGML_TYPE_F32,   model.hparams.n_input, model.hparams.n_hidden);
-        fin.read(reinterpret_cast<char *>(model.fc1_weight->data), ggml_nbytes(model.fc1_weight));
-
-        int32_t ne_bias[2] = { 1, 1 };
-        for (int i = 0; i < n_dims; ++i) {
-            fin.read(reinterpret_cast<char *>(&ne_bias[i]), sizeof(ne_bias[i]));
+            model.fc1_bias = ggml_new_tensor_1d(ctx, GGML_TYPE_F32, model.hparams.n_hidden);
+            fin.read(reinterpret_cast<char *>(model.fc1_bias->data), ggml_nbytes(model.fc1_bias));
+            ggml_set_name(model.fc1_bias, "fc1_bias");
         }
-
-        model.fc1_bias    = ggml_new_tensor_1d(ctx, GGML_TYPE_F32, model.hparams.n_hidden);
-        fin.read(reinterpret_cast<char *>(model.fc1_bias->data), ggml_nbytes(model.fc1_bias));
     }
 
     // Read FC2 layer 2
@@ -127,23 +130,30 @@ bool mnist_model_load(const std::string & fname, mnist_model & model) {
         int32_t n_dims;
         fin.read(reinterpret_cast<char *>(&n_dims), sizeof(n_dims));
 
-        int32_t ne_weight[2] = { 1, 1 };
-        for (int i = 0; i < n_dims; ++i) {
-            fin.read(reinterpret_cast<char *>(&ne_weight[i]), sizeof(ne_weight[i]));
+        {
+            int32_t ne_weight[2] = { 1, 1 };
+            for (int i = 0; i < n_dims; ++i) {
+                fin.read(reinterpret_cast<char *>(&ne_weight[i]), sizeof(ne_weight[i]));
+            }
+
+            // FC1 dimensions taken from file, eg. 10x500
+            model.hparams.n_classes = ne_weight[1];
+
+            model.fc2_weight = ggml_new_tensor_2d(ctx, GGML_TYPE_F32, model.hparams.n_hidden, model.hparams.n_classes);
+            fin.read(reinterpret_cast<char *>(model.fc2_weight->data), ggml_nbytes(model.fc2_weight));
+            ggml_set_name(model.fc2_weight, "fc2_weight");
         }
 
-        // FC1 dimensions taken from file, eg. 10x500
-        model.hparams.n_classes = ne_weight[1];
+        {
+            int32_t ne_bias[2] = { 1, 1 };
+            for (int i = 0; i < n_dims; ++i) {
+                fin.read(reinterpret_cast<char *>(&ne_bias[i]), sizeof(ne_bias[i]));
+            }
 
-        model.fc2_weight     = ggml_new_tensor_2d(ctx, GGML_TYPE_F32, model.hparams.n_hidden, model.hparams.n_classes);
-        fin.read(reinterpret_cast<char *>(model.fc2_weight->data), ggml_nbytes(model.fc2_weight));
-
-        int32_t ne_bias[2] = { 1, 1 };
-        for (int i = 0; i < n_dims; ++i) {
-            fin.read(reinterpret_cast<char *>(&ne_bias[i]), sizeof(ne_bias[i]));
+            model.fc2_bias = ggml_new_tensor_1d(ctx, GGML_TYPE_F32, model.hparams.n_classes);
+            fin.read(reinterpret_cast<char *>(model.fc2_bias->data), ggml_nbytes(model.fc2_bias));
+            ggml_set_name(model.fc2_bias, "fc2_bias");
         }
-        model.fc2_bias     = ggml_new_tensor_1d(ctx, GGML_TYPE_F32, model.hparams.n_classes);
-        fin.read(reinterpret_cast<char *>(model.fc2_bias->data), ggml_nbytes(model.fc2_bias));
     }
     fin.close();
 
@@ -178,6 +188,7 @@ int mnist_eval(
 
     struct ggml_tensor * input = ggml_new_tensor_1d(ctx0, GGML_TYPE_F32, hparams.n_input);
     memcpy(input->data, digit.data(), ggml_nbytes(input));
+    ggml_set_name(input, "input");
 
     // fc1 MLP = Ax + b
     ggml_tensor * fc1 = ggml_add(ctx0, ggml_mul_mat(ctx0, model.fc1_weight, input), model.fc1_bias);
